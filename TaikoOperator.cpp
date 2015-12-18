3dab9be2b3e5c5f258c7473459e1ad5dc80b7831
ryukau <ryukau@gmail.com>
2015-12-18 13:50:05 +0900

    不要な機能を削除・整理
    - 変調はPMのみ。他の変調は効果がいまいち。
    - エンベロープの削減
    - RangedFloatの除去

--- TaikoOperator.cpp
+++ TaikoOperator.cpp
@@ -2,21 +2,15 @@
 
 #include <cmath>
 #include <cfloat>
-#include "PitchCalc.h"
-#include "Globals.h"
+#include "utils.h"
 
-TaikoOperator::TaikoOperator() :
-    mute(false),
-    Frequency(0.0, -FLT_MAX, FLT_MAX),
-    ModIndex(0.0),
-    Feedback(0.0),
-    buffer(0.0),
-    phase(0.0),
-    input(0.0),
-    lfo(false),
-    modType(ModulationType::PM),
-    _filterType(FilterType::Bypass),
-    _filter(nullptr)
+TaikoOperator::TaikoOperator()
+    : mute(false)
+    , frequency(0.0)
+    , modIndex(0.0)
+    , buffer(0.0)
+    , phase(0.0)
+    , input(0.0)
 {
 }
 
@@ -33,79 +27,10 @@ TaikoOperator& TaikoOperator::operator=(TaikoOperator &op)
     envAmp = op.envAmp;
     envPitch = op.envPitch;
     envShape = op.envShape;
-    envPhaseOffset = op.envPhaseOffset;
-    envNoise = op.envNoise;
-    envCutoff = op.envCutoff;
-    envResonance = op.envResonance;
-    setFilterType(op.filterType());
-    Frequency = op.Frequency;
-    Feedback = op.Feedback;
-    ModIndex = op.ModIndex;
+    frequency = op.frequency;
+    modIndex = op.modIndex;
 
     return *this;
-}
-
-
-//
-// filter
-//
-
-void TaikoOperator::setFilterType(FilterType type)
-{
-    switch (type)
-    {
-    case FilterType::Moog:
-        _filter = &moogFilter;
-        break;
-
-    case FilterType::Karlsen:
-        _filter = &karlsenFilter;
-        break;
-
-    case FilterType::EMSVCS3:
-        _filter = &emsVcs3Filter;
-        break;
-
-    case FilterType::MovingAverage:
-        _filter = &movingAverageFilter;
-        break;
-
-    case FilterType::TB18dB:
-        _filter = &tb18dbFilter;
-        break;
-
-    case FilterType::Bypass:
-    default:
-        _filter = nullptr;
-    }
-
-    _filterType = type;
-}
-
-TaikoOperator::ModulationType TaikoOperator::modulationType()
-{
-    return modType;
-}
-
-void TaikoOperator::setModulationType(TaikoOperator::ModulationType type)
-{
-    modType = type;
-}
-
-void TaikoOperator::setLFO(bool flag)
-{
-    lfo = flag;
-}
-
-bool TaikoOperator::isLFO()
-{
-    return lfo;
-}
-
-
-TaikoOperator::FilterType TaikoOperator::filterType()
-{
-    return _filterType;
 }
 
 
@@ -118,32 +43,24 @@ void TaikoOperator::setModInput(float input)
     this->input = input;
 }
 
+void TaikoOperator::setFrequency(float freq)
+{
+    frequency = freq;
+}
+
+void TaikoOperator::setModIndex(float index)
+{
+    modIndex = index;
+}
+
+
 void TaikoOperator::clearBuffer()
 {
     buffer = 0.0;
     phase = 0.0;
     input = 0.0;
-
-    if (RangedFloat::state == RangedFloat::State::Mid)
-    {
-        Frequency.setMid();
-        ModIndex.setMid();
-        Feedback.setMid();
-
-        envAmp.setMid();
-        envPitch.setMid();
-        envShape.setMid();
-        envPhaseOffset.setMid();
-        envNoise.setMid();
-        envCutoff.setMid();
-        envResonance.setMid();
-    }
-
-    moogFilter.clearBuffer();
-    tb18dbFilter.clearBuffer();
-    karlsenFilter.clearBuffer();
-    emsVcs3Filter.clearBuffer();
 }
+
 
 void TaikoOperator::clearBuffer(float phase)
 {
@@ -151,74 +68,18 @@ void TaikoOperator::clearBuffer(float phase)
     this->phase = phase;
 }
 
-float TaikoOperator::render(float time, float frequencyMul, float frequencyAdd, float frequencyMod)
+
+float TaikoOperator::render(float time)
 {
-    if (mute || (envAmp.AttackTime.get() + envAmp.DecayTime.get()) < time)
+    if (mute)
         return 0.0;
 
     osc.setShape(envShape.at(time));
-    osc.setPhaseOffset(envPhaseOffset.at(time));
-    osc.setNoiseRatio(envNoise.at(time));
 
-    float frequency;
-    if (lfo)
-        frequency = Frequency.get() + PitchCalc::intervalToFreq(envPitch.at(time) * 10000);
-    else
-        frequency = fmod((frequencyMul * Frequency.get()) + frequencyAdd + PitchCalc::intervalToFreq(envPitch.at(time) * 10000), frequencyMod);
+    // PM変調
+    phase += (frequency + (PitchCalc::intervalToFreq(envPitch.at(time) * 10000))) / SampleRate::get();
 
-    switch (modType)
-    {
-    case ModulationType::PM:
-        buffer = Pm(time, frequency);
-        break;
-    case ModulationType::FM:
-        buffer = Fm(time, frequency);
-        break;
-    case ModulationType::AM:
-        buffer = Am(time, frequency);
-        break;
-    }
-
-    if (_filter)
-    {
-        _filter->setCutoff(envCutoff.at(time));
-        _filter->setResonance(envResonance.at(time));
-        buffer = _filter->pass(buffer);
-    }
+    buffer = envAmp.at(time) * osc.Oscillation(phase + 8 * modIndex * input);
 
     return buffer;
-}
-
-
-//
-// private
-//
-float TaikoOperator::Pm(float time, float frequency)
-{
-    float modIndex = 8.0f * ModIndex.get();//pow(\d+\.\d+f, ModIndex.get()) - \d+\.\d+f;
-    float feedback = pow(1.5f, Feedback.get()) - 1.0f;
-
-    phase += frequency / Globals::SampleRate();
-    return envAmp.at(time) * osc.Oscillation(phase + modIndex * input + feedback * buffer);
-}
-
-float TaikoOperator::Fm(float time, float frequency)
-{
-    float modIndex = frequency * ModIndex.get();//pow(frequency, ModIndex.get());//\d+\.\d+f * ModIndex.get();
-    float feedback = frequency * 0.99f * Feedback.get();
-
-    phase += (frequency + (modIndex * input + feedback * buffer)) / Globals::SampleRate();
-    return envAmp.at(time) * osc.Oscillation(phase); // fm
-}
-
-float TaikoOperator::Am(float time, float frequency)
-{
-    float feedback = frequency * 0.99f * Feedback.get();
-
-    phase += (frequency + feedback * buffer) / Globals::SampleRate();
-
-    float car = envAmp.at(time) * osc.Oscillation(phase);
-    float mod = input;
-
-    return ModIndex.get() * car * mod + (1.0f - ModIndex.get()) * car;
 }
--- TaikoOperator.h
+++ TaikoOperator.h
@@ -2,35 +2,9 @@
 
 #include "Oscillator.h"
 #include "Envelope.h"
-#include "Filter/MoogFilter.h"
-#include "Filter/TB18dBFilter.h"
-#include "Filter/KarlsenFilter.h"
-#include "Filter/EMSVCS3Filter.h"
-#include "Filter/LinearPhaseDCBlocker.h"
-
 
 class TaikoOperator
 {
-public:
-    enum class FilterType : int
-    {
-        Bypass = 0,
-        Moog,
-        Karlsen,
-        EMSVCS3,
-        MovingAverage,
-        TB18dB,
-        Last = TB18dB,
-    };
-
-    enum class ModulationType : int
-    {
-        PM = 0,
-        FM,
-        AM,
-        Last,
-    };
-
 public:
     TaikoOperator();
     ~TaikoOperator();
@@ -43,52 +17,19 @@ public:
     Envelope envAmp;
     Envelope envPitch;
     Envelope envShape;
-    Envelope envPhaseOffset;
-    Envelope envNoise;
 
-    Envelope envCutoff;
-    Envelope envResonance;
-
-    // filter
-    FilterType filterType();
-    void setFilterType(FilterType type);
-
-    // pitch
-    RangedFloat Frequency;
-
-    // modulation
-    RangedFloat ModIndex;
-    RangedFloat Feedback;
-
-    ModulationType modulationType();
-    void setModulationType(ModulationType type);
-
-    void setLFO(bool flag);
-    bool isLFO();
-
-    // render
     void setModInput(float input);
+    void setFrequency(float freq);
+    void setModIndex(float index);
     void clearBuffer();
     void clearBuffer(float phase);
-    float render(float time, float frequencyMul, float frequencyAdd, float frequencyMod);
+    float render(float time);
 
 private:
-    ModulationType modType;
     float buffer;
     float phase;
     float input;
-    bool lfo;
-
-    FilterType _filterType;
-    Filter * _filter;
-    MoogFilter moogFilter;
-    KarlsenFilter karlsenFilter;
-    TB18dBFilter tb18dbFilter;
-    EMSVCS3Filter emsVcs3Filter;
-    LinearPhaseDCBlocker movingAverageFilter;
-
-    float Pm(float time, float frequency);
-    float Fm(float time, float frequency);
-    float Am(float time, float frequency);
+    float frequency;
+    float modIndex;
 };
 
